362816ec46d76fed3eb2d1493fa1f167d5cf6635
Ezzeldien Magdy <139116899+Ezzeldien-M-Abdalaziz@users.noreply.github.com>
2025-10-10 11:32:11 +0300

    edit flag problem

--- resources/views/layouts/dashboard/_header.blade.php
+++ resources/views/layouts/dashboard/_header.blade.php
@@ -182,7 +182,7 @@
 
             <li class="dropdown dropdown-language nav-item">
               <a class="dropdown-toggle nav-link" id="dropdown-flag" href="#" data-toggle="dropdown"
-              aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-gb"></i><span class="selected-language"></span></a>
+              aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-@if(config('app.locale') == 'ar') flag-icon-eg @else flag-icon-gb @endif"></i><span class="selected-language"></span></a>
               <div class="dropdown-menu" aria-labelledby="dropdown-flag">
               @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
                 
