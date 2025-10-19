a5ba91d7f65fc6bb43a1251e418fcf381e95b198
Ezzeldien Magdy <139116899+Ezzeldien-M-Abdalaziz@users.noreply.github.com>
2025-10-19 22:08:04 +0300

    update lang files

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -16,5 +16,14 @@ return [
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'remember_me'=>'Remember Me',
+    'forget_password'=>'  Forgot Password',
+    'login'=>'Login',
+    'email'=>'Email ',
+    'password'=>'Password',
+    'confirm_password'=>'Confirm Password ',
+    'logout'=>'Logout',
+    'name'=>'Name',
+    'not_match'=>'Crediensials dose not match',
 
 ];
